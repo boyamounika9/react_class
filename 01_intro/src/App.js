e29770d337531ea3aaad03b9655cc905393e9f79
mounika <189mounika.b@gmail.com>
2026-09-25 12:23:15 +0530

    adding new project for styling

---
+++ 01_intro/src/App.js
@@ -0,0 +1,10 @@
+
+import First from "./componants/First"
+
+function App() {
+  return (
+    <First/>
+  );
+}
+
+export default App;
---
+++ 01_intro/src/componants/First.jsx
@@ -0,0 +1,20 @@
+function name(){
+    let a=10;
+    console.log(a)
+    let name ="JEEVAN"
+    return (
+        <>
+            <img src="" alt="" />
+            <label htmlFor=""></label>
+            <input type="text" />
+            <br />
+            {1+3}
+            <h1>HII HELOO NAMASTE {name} GARUU</h1>
+
+           {console.log("hello")}
+            <h1></h1>
+        </>
+    )
+}
+
+export default name;
